2f4a2f27ec25db13a1d5c77d63777264b6f60250
Emil Fejzagić <efejzagic2@etf.unsa.ba>
2023-05-18 17:20:45 +0200

    Init of Authentication Service with Keycloak and JWT

---
+++ CostTrackingApi/AuthService/Controllers/AuthController.cs
@@ -0,0 +1,61 @@
+﻿using AuthService.Models;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.Net.Http.Headers;
+using System.Security.Claims;
+using Newtonsoft.Json;
+
+namespace AuthService.Controllers
+{
+    [ApiController]
+    [Authorize]
+    [Route("api/[controller]")]
+    public class AuthController : ControllerBase
+    {
+        private readonly IHttpContextAccessor _httpContextAccessor;
+
+        public AuthController(IHttpContextAccessor httpContextAccessor)
+        {
+            _httpContextAccessor = httpContextAccessor;
+        }
+
+        [HttpGet]
+        [Route("Id")]
+        public string SomeMethod()
+        {
+            var userId = _httpContextAccessor.HttpContext.User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+            // Use the user ID or other claims as needed
+            return userId;
+        }
+        [HttpGet]
+        [Route("All")]
+        public async Task<List<User>> GetAllUsers()
+        {
+            var accessToken = _httpContextAccessor.HttpContext.GetTokenAsync("access_token").Result;
+            var requestUrl = "https://your-keycloak-server/auth/admin/realms/your-realm/users";
+
+            using (var httpClient = new HttpClient())
+            {
+                httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", accessToken);
+
+                var response = await httpClient.GetAsync(requestUrl);
+
+                if (response.IsSuccessStatusCode)
+                {
+                    var responseContent = await response.Content.ReadAsStringAsync();
+                    var users = JsonConvert.DeserializeObject<List<User>>(responseContent);
+                    return users;
+                }
+                else
+                {
+                    // Handle error response
+                    // For example, log the error or throw an exception
+                    // throw new Exception("Failed to retrieve users");
+                }
+            }
+
+            return null;
+        }
+    }
+}
---
+++ CostTrackingApi/AuthService/Program.cs
@@ -0,0 +1,96 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
+using System.Text;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+
+builder.Services.AddControllers();
+
+builder.Services.AddAuthentication(options =>
+{
+options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+})
+       .AddJwtBearer(options =>
+        {
+               options.Authority = "https://lemur-5.cloud-iam.com/auth/realms/cost-tracking-app/";
+               options.Audience = "your-audience";
+               options.TokenValidationParameters = new TokenValidationParameters
+               {
+                   ValidateIssuer = true,
+                   ValidateAudience = true,
+                   ValidateLifetime = true,
+                   ValidateIssuerSigningKey = true,
+                   ClockSkew = TimeSpan.Zero,
+                   ValidIssuer = "https://lemur-5.cloud-iam.com/auth/realms/cost-tracking-app/",
+                   ValidAudience = "your-audience",
+                   IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes("your-signing-key"))
+               };
+           });
+
+
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+var securityScheme = new OpenApiSecurityScheme()
+{
+    Name = "Authorization",
+    Type = SecuritySchemeType.ApiKey,
+    Scheme = "Bearer",
+    BearerFormat = "JWT",
+    In = ParameterLocation.Header,
+    Description = "JSON Web Token based security",
+};
+
+var securityReq = new OpenApiSecurityRequirement()
+{
+    {
+        new OpenApiSecurityScheme
+        {
+            Reference = new OpenApiReference
+            {
+                Type = ReferenceType.SecurityScheme,
+                Id = ""
+            }
+        },
+        new string[] {}
+    }
+};
+
+
+
+var info = new OpenApiInfo()
+{
+    Version = "v1",
+    Title = "Keycloak & JWT Integration",
+    Description = "Implementing JWT Authentication",
+};
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(o =>
+{
+    o.SwaggerDoc("v1", info);
+    o.AddSecurityDefinition("Bearer", securityScheme);
+    o.AddSecurityRequirement(securityReq);
+});
+
+
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+app.UseAuthentication();
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
