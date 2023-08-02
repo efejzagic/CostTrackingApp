03052881d7186acaeca5cf81f984cfa868f42c60
Emil Fejzagić <efejzagic2@etf.unsa.ba>
2023-08-02 18:17:54 +0200

    ConstructionSite Service docker init

--- CostTrackingApi/ConstructionSite.WebAPI/obj/Debug/net7.0/ConstructionSite.WebAPI.AssemblyInfo.cs
+++ CostTrackingApi/ConstructionSite.WebAPI/obj/Debug/net7.0/ConstructionSite.WebAPI.AssemblyInfo.cs
@@ -11,6 +11,7 @@
 using System;
 using System.Reflection;
 
+[assembly: Microsoft.Extensions.Configuration.UserSecrets.UserSecretsIdAttribute("180e222e-9353-496e-831f-df49923e85d3")]
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConstructionSite.WebAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
